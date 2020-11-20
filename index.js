4c767818e8ff9a73398feba1ecac616e33171d5c
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-11-20 10:01:27 +0530

    changes

--- index.js
+++ index.js
@@ -130,49 +130,49 @@ function checkSquare(square, currentId) {
   setTimeout(() => {
     if (currentId > 0 && !isLeftEdge) {
       const newId = squares[currentId - 1].id;
-      //const newId = parseInt(currentId) - 1   ....refactor
+      //const newId = (currentId) - 1   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
     if (currentId > 9 && !isRightEdge) {
       const newId = squares[currentId + 1 - width].id;
-      //const newId = parseInt(currentId) +1 -width   ....refactor
+      //const newId = (currentId) +1 -width   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
     if (currentId > 10) {
       const newId = squares[currentId - width].id;
-      //const newId = parseInt(currentId) -width   ....refactor
+      //const newId = (currentId) -width   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
     if (currentId > 11 && !isLeftEdge) {
       const newId = squares[currentId - 1 - width].id;
-      //const newId = parseInt(currentId) -1 -width   ....refactor
+      //const newId = (currentId) -1 -width   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
     if (currentId < 98 && !isRightEdge) {
       const newId = squares[currentId + 1].id;
-      //const newId = parseInt(currentId) +1   ....refactor
+      //const newId = (currentId) +1   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
     if (currentId < 90 && !isLeftEdge) {
       const newId = squares[currentId - 1 + width].id;
-      //const newId = parseInt(currentId) -1 +width   ....refactor
+      //const newId = (currentId) -1 +width   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
     if (currentId < 88 && !isRightEdge) {
       const newId = squares[currentId + 1 + width].id;
-      //const newId = parseInt(currentId) +1 +width   ....refactor
+      //const newId = (currentId) +1 +width   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
     if (currentId < 89) {
       const newId = squares[currentId + width].id;
-      //const newId = parseInt(currentId) +width   ....refactor
+      //const newId = (currentId) +width   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
